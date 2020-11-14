f7ceab8501822a2bb3fb57eee51c4cf592dbb863
Enes Yilmaz <enesyilmaz60@hotmail.com>
2020-11-14 16:59:32 +0100

    bootstrap and css in react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import User from "./components/User"
 import './App.css';
 
 function App() {
   const test = 34;
   const isAuth = true;
   return (// yalnizca bir tane parent element yazabiliriz icine örengin burda bir div var ve altina istedigimiz kadar children element ekleyebiliirz ancak ikinci bir ana element olusturamayiz.
-    <div className="App">
+    <div className="container">
+      <h4>App Component</h4>
+
       {/* <h4>{1+1}</h4>
       <h4>{"murat".toUpperCase()}</h4>
       <h4>{test}</h4>
@@ -15,14 +16,17 @@ function App() {
           : null
         }
       </div> */}
-      <h4>App Component</h4>
+      {/* <h4 style={{color:'blue',fontSize:"30px"}}>App Component</h4> */}
+      {/* <User/>
       <User/>
-      <User/>
-      <User/>
+      <User/> */}
     </div>
   );
 }
 // class degil JSX te className
 // labeldaki for JSX te htmlFor
 // JSX icinde {} icine direk javascript hesaplamalari yazilabilir.
+// inline css yukardaki gibi :  style={{color:'blue',fontSize:"30px"}}
+// app.css icinde de css external css yazabiliriz
+// bootstrap cdn ile index html icine yazilabilir. npm ile de yüklenebilir.
 export default App;
