ac0f41842f9ca96b94806d53d262005e5b7ad93c
Enes Yilmaz <enesyilmaz60@hotmail.com>
2020-11-14 16:38:11 +0100

    if in JSX

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@
 import './App.css';
 
 function App() {
-  return (
-    <h1>Hello React</h1>
+  const test = 34;
+  const isAuth = true;
+  return (// yalnizca bir tane parent element yazabiliriz icine örengin burda bir div var ve altina istedigimiz kadar children element ekleyebiliirz ancak ikinci bir ana element olusturamayiz.
+    <div className="App">
+      <h4>{1+1}</h4>
+      <h4>{"murat".toUpperCase()}</h4>
+      <h4>{test}</h4>
+      <div>
+        {
+          isAuth ? <p>Kullanici kayitli</p>
+          : null
+        }
+      </div>
+    </div>
   );
 }
-
+// class degil JSX te className
+// labeldaki for JSX te htmlFor
+// JSX icinde {} icine direk javascript hesaplamalari yazilabilir.
 export default App;
