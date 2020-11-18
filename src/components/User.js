c9c1ce123738ac8bf750dab64647e7074d2a0c33
Enes Yilmaz <enesyilmaz60@hotmail.com>
2020-11-18 17:46:37 +0100

    react event

--- src/components/User.js
+++ src/components/User.js
@@ -28,6 +28,21 @@ class User extends Component {
     salary : "Keine Information",
     department : "Keine Information"
   }
+  // // bind islemini asagida yapmazsak jsx te, constructor icinde de yapabiliriz.
+  // constructor(props){
+  //   super(props);
+  //   this.onClickEvent = this.onClickEvent.bind(this);
+  // }
+  // arrow functionla bind islemi yapmadan calistirdik ve user objemiz geldi.
+  onClickEvent = (e) => {
+    console.log(this);
+    console.log("test");
+  }
+
+  // onClickEvent(e){ // onclick sonrasi asagida bind yaparsak bu sekilde
+  //   console.log(this);
+  //   console.log("test");
+  // }
   // state 2. yöntem
   // constructor (props){ // staate bu sekilde de olusturulabilir ya da satatic teki gibi direk olrak ta olusturulabilir
   //   super(props);
@@ -44,7 +59,7 @@ class User extends Component {
       <div className = "col-md-8 mb-4">
         <div className="card">
           <div className="card-header d-flex justify-content-between">
-            <h4 className="d-inline">{name}</h4>
+            <h4 className="d-inline" onClick={this.onClickEvent}>{name}</h4>
             <i className="far fa-trash-alt" style={{cursor : "pointer"}}></i>
           </div>
           { isVisible ?
@@ -72,3 +87,8 @@ User.propTypes = {
 }
 
 export default User;
+
+
+// click islemmi gibi islemler yaoinca ya bind(this) yapmamiz gerekir, yada finksiyonu arrow function yapmak gerekir. cumku arrow function otomatik bind yapmis olur.
+//jsx kisminda bind islmi yaparken deger gönderebiliyoruz. z.B .bind(this, 34) ilk basta hep this olmali sonra deger. biz bu degeri
+// yukarda funtion da alabiliriz. onClickEvent = (number,e)=>{console.log(number)} buradaki gibi e degeri en son olmali. burda 34 ciktimizi aliriz
